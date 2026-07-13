d34581207ee408feaa617e53044e6407163785e3
AshkanRN <ashkanrafiei8003@gmail.com>
2026-07-13 05:50:15 +0330

    improve dataloader.py

--- src/dataset/dataloader.py
+++ src/dataset/dataloader.py
@@ -1,19 +1,20 @@
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 
-from .fairface import FairFaceDataset
+from .fairface_dataset import FairFaceDataset
 
 
-
-def get_dataloaders(batch_size=32, transform=None, num_workers=4):
-
+def get_dataset(train_transform=None, val_transform=None):
     dataset = load_dataset("HuggingFaceM4/FairFace", "0.25")
 
+    train_dataset = FairFaceDataset(dataset["train"], transform=train_transform)
+    val_dataset = FairFaceDataset(dataset["validation"], transform=val_transform)
 
-    train_dataset = FairFaceDataset(dataset["train"], transform=transform)
-    val_dataset = FairFaceDataset(dataset["validation"], transform=transform)
+    return train_dataset, val_dataset
 
 
+
+def get_dataloaders(train_dataset, val_dataset, batch_size=32, transform=None, num_workers=4):
     train_loader = DataLoader(
         train_dataset,
         batch_size=batch_size,
