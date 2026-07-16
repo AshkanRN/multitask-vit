ad80938b8a2918345eab53c172b5cb4c8a9c3d44
AshkanRN <ashkanrafiei8003@gmail.com>
2026-07-16 06:37:47 +0330

    implement the main.ipynb file

--- src/fairface_vit.py
+++ src/fairface_vit.py
@@ -2,7 +2,7 @@ from torch import nn
 
 
 class FairFaceViT(nn.Module):
-    def __init__(self, model):
+    def __init__(self, model, dropout=0.15, age_hidden_dim=384):
         super().__init__()
 
         self.backbone = model
@@ -24,10 +24,10 @@ class FairFaceViT(nn.Module):
         self.gender = nn.Linear(hidden_dim, 1)
             
         self.age = nn.Sequential(
-            nn.Linear(768,384),
+            nn.Linear(hidden_dim, age_hidden_dim),
             nn.GELU(),
-            nn.Dropout(0.15),
-            nn.Linear(384,9)
+            nn.Dropout(dropout),
+            nn.Linear(age_hidden_dim, 9)
         )
         self.race = nn.Linear(hidden_dim, 7)
         
